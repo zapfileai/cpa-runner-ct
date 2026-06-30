1788ef772944a88532568e8b5ea3fca692d210ea
Sreehari Annam <sannam@wescodist.com>
2026-06-30 07:19:06 -0400

    feat: load first 80 cities from cities.csv

--- cities.js
+++ cities.js
@@ -1,5 +1,20 @@
-﻿const connecticutCities = [
-  // paste first 80 cities from cities.csv here
+const connecticutCities = [
+  "Amston", "Andover", "Ansonia", "Ashford", "Avon",
+  "Bantam", "Barkhamsted", "Beacon Falls", "Berlin", "Bethany",
+  "Bethel", "Bethlehem", "Bloomfield", "Bolton", "Botsford",
+  "Bozrah", "Branford", "Bridgeport", "Bridgewater", "Bristol",
+  "Broad Brook", "Brookfield", "Brooklyn", "Burlington", "Canaan",
+  "Canterbury", "Canton", "Centerbrook", "Chaplin", "Cheshire",
+  "Chester", "Clinton", "Colchester", "Collinsville", "Columbia",
+  "Cornwall Bridge", "Cos Cob", "Coventry", "Cromwell", "Danbury",
+  "Danielson", "Darien", "Dayville", "Deep River", "Derby",
+  "Durham", "East Berlin", "East Canaan", "East Glastonbury", "East Granby",
+  "East Haddam", "East Hampton", "East Hartford", "East Hartland", "East Haven",
+  "East Lyme", "East Windsor", "East Windsor Hill", "Eastford", "Easton",
+  "Ellington", "Enfield", "Essex", "Fairfield", "Farmington",
+  "Gales Ferry", "Georgetown", "Glastonbury", "Goshen", "Granby",
+  "Greens Farms", "Greenwich", "Groton", "Guilford", "Haddam",
+  "Hamden", "Hampton", "Hartford", "Harwinton", "Hawleyville",
 ];
 
 const BATCH_SIZE = 5;
